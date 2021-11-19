604dbc798d45211ceeb924e429d4936b7575f496
박연배 <yeonpark@bag-yeonbaes-MacBook-Air.local>
2021-11-19 15:57:16 +0900

    UI 수정, 이니셜 뷰 컨트롤러 로직 수정, AddPinView UI 작성

--- Mappin/Controller/AddPinViewController.swift
+++ Mappin/Controller/AddPinViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealmSwift
+import GoogleMaps
 
 class AddPinViewController: UIViewController {
 
@@ -17,11 +18,31 @@ class AddPinViewController: UIViewController {
     
     //MARK: UI
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var documentTitleTextField: UITextField!
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    @IBOutlet weak var contentTextView: UITextView!
+    
     
     
     //MARK: Method
     @objc func addPin(_ sender: UIBarButtonItem) {
         print("DEBUG: 핀 추가")
+    }
+    
+    func mapViewConfig() {
+        mapView.settings.scrollGestures = false
+        mapView.settings.zoomGestures = false
+        mapView.settings.tiltGestures = false
+        mapView.settings.rotateGestures = false
+        
+        mapView.isMyLocationEnabled = true
     }
     
     func navBarConfig() {
@@ -34,12 +55,59 @@ class AddPinViewController: UIViewController {
         self.title = "핀 추가"
     }
     
+    func collectionViewConfig() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let nib = UINib(nibName: PhotoCollectionViewCell.identifier, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        flowLayout.minimumInteritemSpacing = 10
+        flowLayout.scrollDirection = .horizontal
+        
+        collectionView.collectionViewLayout = flowLayout
+        
+    }
+    
     
     //MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navBarConfig()
+        collectionViewConfig()
+        mapViewConfig()
     }
     
 
+}
+
+//MARK: CollectionView Delegate
+
+extension AddPinViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as? PhotoCollectionViewCell else {
+            print("DEBUG: 셀 변환 실패함 ㅋㅋ")
+            return UICollectionViewCell()
+        }
+        
+        cell.photoImageView.image = UIImage(systemName: "camera")
+        cell.photoImageView.backgroundColor = .blue
+        cell.layer.cornerRadius = 10
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let height = collectionView.frame.height - 20
+        
+        return CGSize(width: height, height: height)
+    }
+    
 }
